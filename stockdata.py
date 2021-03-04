03a661ce01a56962a04c9db8655c96efc0d22a07
Jon Trost <jontrost426@gmail.com>
2021-03-04 00:19:34 -0600

    Added option for user to input stock ticker

--- stockdata.py
+++ stockdata.py
@@ -4,8 +4,15 @@ import json
 def get_stock_price(stock):
     source = request.urlopen(f"https://financialmodelingprep.com/api/v3/quote/{stock}?apikey=ea998f3c010a894ff2f028237e4bd572").read()
     data = json.loads(source)
-    price = data[0]['price']
-    return price
+    if not data:
+        return 0
+    else: 
+        return data[0]['price']
 
-price = get_stock_price("GME")
-print(f"GME price = {price}")
+while True:
+    ticker_symbol = input("Enter a stock ticker symbol to receive price data: ").upper()
+    price = get_stock_price(ticker_symbol)
+    if price == 0:
+        print(f"The stock ticker symbol '{ticker_symbol}' is invalid")
+    else:
+        print(f"{ticker_symbol} price = {price}")
